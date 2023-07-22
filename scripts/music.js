b4b1306a05a635c442bc99231e8653862c1a2265
Max Leeming <max.leeming@gmail.com>
2023-07-22 11:06:25 -0400

    Tracks scroll into view / can be linked

--- scripts/music.js
+++ scripts/music.js
@@ -7,9 +7,13 @@ fetch('/scripts/music.json')
 let $tableDiv = document.getElementById('tableDiv')
 let $clypDiv = document.getElementById('clypDiv')
 
-function loadClyp(code) {
+function loadClyp(index) {
+    let code = music[index].code
     if (code) {
         $clypDiv.innerHTML = '<iframe width="100%" height="200" src="https://clyp.it/' + code + '/widget" frameborder="0"></iframe>'
+
+        if ($tableDiv.getElementsByClassName('selected')[0]) $tableDiv.getElementsByClassName('selected')[0].classList.remove('selected')
+        document.getElementById('music-' + index).classList.add('selected')
     } else {
         console.log('No code!')
     }
@@ -21,10 +25,15 @@ function loadTable() {
         let urlName = makeURLFriendly(music[i].name)
         let output = ''
         output += "<a href=#" + urlName + " id='music-" + i + "'><div data-id='" + i + "'>"
-        output += music[i].name
-        output += "<span class='tag'>" + music[i].length + "</span>"
-        output += "<span class='tag'>" + music[i].year + "</span>"
-        output += "<span class='tag'>" + music[i].album + "</span>"
+        
+        output += "<span class='title'>" + music[i].name
+        music[i].favourite ? output += "<span class='favourite'>Favourite!</span>" : ""
+        output += "</span>"
+        
+        output += "<span class='tag'>" + (music[i].album ? music[i].album : "-") + "</span>"
+        
+        output += "<span class='tag'>" + (music[i].year ? music[i].year : "-")  + "</span>"
+        output += "<span class='tag'>" + (music[i].length ? music[i].length : "-") + "</span>"
         output += "</div></a>"
         $tableDiv.innerHTML += output
     }
@@ -38,22 +47,38 @@ function loadTable() {
     
 // }
 
-function selectTrack(e) {
-    let $clicked = e.target
-    if ($clicked.dataset.id) {
-        loadClyp(music[$clicked.dataset.id].code)
-        // scrollToRecipe()
+function scrollToClyp() {
+    // if (document.body.clientWidth <= 700)
+    document.getElementById("top").scrollIntoView({ behavior: 'smooth' });
+}
 
-        if ($tableDiv.getElementsByClassName('selected')[0]) $tableDiv.getElementsByClassName('selected')[0].classList.remove('selected')
-        document.getElementById('music-' + $clicked.dataset.id).classList.add('selected')
+function selectTrack(e) {
+    let $clicked = e.target.closest('div')
+    if ($clicked.dataset.id) {
+        loadClyp($clicked.dataset.id)
+        scrollToClyp()
+
     }
 }
 
+function selectTrackFromURL() {
+    if (window.location.hash) {
+        for (let i = 0; i < music.length; i++) {
+            if (music[i] && music[i].name && '#' + makeURLFriendly(music[i].name) == window.location.hash) {
+                loadClyp(i)
+                break
+            }
+        }
+    } else {
+        loadClyp(0)
+    }
+}
 
 function init() {
     loadTable()
-    loadClyp(music[0].code)
     document.getElementById('music-0').classList.add('selected')
+
+    selectTrackFromURL()
 
     $tableDiv.addEventListener("click", selectTrack)
 }
