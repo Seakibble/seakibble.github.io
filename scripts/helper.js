82c1eadf70fd98b59ebb15858180cdef35f5084f
Max Leeming <max.leeming@gmail.com>
2023-07-25 17:21:05 -0400

    Removed space-wrapped dashes in URL friendly links

--- scripts/helper.js
+++ scripts/helper.js
@@ -1,11 +1,12 @@
-const removeChars = [',',"'","!","&","?","/",'"',"(",")"]
+const removeChars = ['- ',',',"'","!","&","?","/",'"',"(",")"]
 function makeURLFriendly(text) {
     let output = text
     if (output !== undefined) {
-        output = output.replaceAll(' ', '_')
-        for (let i = 0; i < removeChars.length;i++) {
-            output = output.replaceAll(removeChars[i],'')
+        for (let i = 0; i < removeChars.length; i++) {
+            output = output.replaceAll(removeChars[i], '')
         }
+        output = output.replaceAll(' ', '_')
+        
         output = output.toLowerCase()
     } else output = ''
     return output
