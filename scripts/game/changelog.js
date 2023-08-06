2fadb45a7adbf54db8f11b20a5846590cfd56769
Max Leeming <max.leeming@gmail.com>
2023-08-05 22:18:24 -0400

    v0.9.6 - Moved objects to their own folder

--- scripts/game/changelog.js
+++ scripts/game/changelog.js
@@ -1,5 +1,6 @@
 const CHANGELOG = [
     // { version: '', log: '' },
+    { version: 'v0.9.6', log: 'Moved objects to their own folder' },
     { version: 'v0.9.5', log: 'Text now appears if you can interact' },
     { version: 'v0.9.4', log: 'You can now Interact with things' },
     { version: 'v0.9.3', log: 'Pyre Vector cache bugfix' },
